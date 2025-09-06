02751e0d8b40fd51632cd363c04f1fac410bcd67
Vinayak Patil <vinayakom@gmail.com>
2025-09-06 16:51:43 +0530

    Movies cards details
    Added movies cards in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import React from "react";
+
 export const App = () => {
-  return <h1>Hello World !! My name is Aditi Patil.</h1>;
+  return (
+    <div>
+        <div><img src="saiyaara.jpg" alt="saiyaara.jpg" height="40%" width="40%" /></div>
+        <h2>Name: Saiyaara</h2>
+        <h3>Ratings: 6.4</h3>
+        <p>Vaani and Krish will have to navigate life, their problems, their insecurities to hopefully realise that only love can be the answer to everything.</p>
+    </div>
+  );
 };
