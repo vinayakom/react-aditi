3ac539a034a52718f184acfce8b4e7e595523f92
Vinayak Patil <vinayakom@gmail.com>
2025-09-07 16:23:41 +0530

    Conditional Operations

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,34 @@ export const App = () => {
 };
 
 const MovieCards = () => {
+
+  const movieName = "Saiyaara";
+  const movieRating = "6.4";
+  const movieSummary = "Vaani and Krish will have to navigate life, their problems, their insecurities to hopefully realise that only love can be the answer to everything.";
+
+  const movieGenre = () => {
+    const genre = "Romance";
+    return genre;
+  };
+
+  let age = 19;
+  // let isAbleToWatch = "Not Available";
+  // if(age >= 18) isAbleToWatch = "Watch Now";  
+
+  const isAbleToWatch = () => {
+    if(age >= 18) return "Watch Now";  
+    return "Not Available";
+  };
+
   return (
     <div>
         <div><img src="saiyaara.jpg" alt="saiyaara.jpg" height="40%" width="40%" /></div>
-        <h2>Name: Saiyaara</h2>
-        <h3>Ratings: 6.4</h3>
-        <p>Vaani and Krish will have to navigate life, their problems, their insecurities to hopefully realise that only love can be the answer to everything.</p>
+        <h2>Name: {movieName}</h2>
+        <h3>Ratings: {movieRating}</h3>
+        <p>Genre: {movieGenre()}</p>
+        <p>Summary: {movieSummary}</p>        
+        {/* <button> {age >= 18 ? "Watch Now" : "Not Available"} </button> */}
+        <button>{isAbleToWatch()}</button>
     </div>
   );
 };
