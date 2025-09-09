ed0dabeef0079fc87b473afe430a6d2b42eb6a07
Vinayak Patil <vinayakom@gmail.com>
2025-09-09 16:10:21 +0530

    Prop details updated

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -4,11 +4,7 @@ import { MovieLists } from "./MovieLists";
 export const MovieCards = () => {
   return (
     <ul>
-      {
-        moviesData.map((movie) => {
-          return (<MovieLists movie={movie} key={movie.movieId} />);
-        })
-      }
+      {moviesData.map((movie) => <MovieLists key={movie.movieId} value={movie} />)}
     </ul>
   );
 };
--- src/components/MovieLists.jsx
+++ src/components/MovieLists.jsx
@@ -1,14 +1,14 @@
 export const MovieLists = (movieProps) => {
-    
+
     return (
         <li>
-            <div><img src={movieProps.movie.movieImg_url} alt={movieProps.movie.movieImg_url} height="40%" width="40%" /></div>
-            <h2>Name: {movieProps.movie.movieName}</h2>
-            <h3>Ratings: {movieProps.movie.movieRating}</h3>
-            <p>Genre: {movieProps.movie.movieGenre}</p>
-            <p>Summary: {movieProps.movie.movieDescription}</p>
-            <p>Cast: {movieProps.movie.movieCast}</p>
-            <a href={movieProps.movie.movieWatch_url} target="_blank">
+            <div><img src={movieProps.value.movieImg_url} alt={movieProps.value.movieImg_url} height="40%" width="40%" /></div>
+            <h2>Name: {movieProps.value.movieName}</h2>
+            <h3>Ratings: {movieProps.value.movieRating}</h3>
+            <p>Genre: {movieProps.value.movieGenre}</p>
+            <p>Summary: {movieProps.value.movieDescription}</p>
+            <p>Cast: {movieProps.value.movieCast}</p>
+            <a href={movieProps.value.movieWatch_url} target="_blank">
                 <button>Watch Now</button>
             </a>
         </li>
