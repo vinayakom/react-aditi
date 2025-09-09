dd5617178689109edabcb71c1212a4626f1599b5
Vinayak Patil <vinayakom@gmail.com>
2025-09-09 16:24:50 +0530

    Prop.value replaced with value

--- src/components/MovieLists.jsx
+++ src/components/MovieLists.jsx
@@ -1,14 +1,16 @@
-export const MovieLists = (movieProps) => {
+export const MovieLists = ({value}) => {
+
+    const { movieImg_url, movieName, movieRating, movieGenre, movieDescription, movieCast, movieWatch_url } = value;
 
     return (
         <li>
-            <div><img src={movieProps.value.movieImg_url} alt={movieProps.value.movieImg_url} height="40%" width="40%" /></div>
-            <h2>Name: {movieProps.value.movieName}</h2>
-            <h3>Ratings: {movieProps.value.movieRating}</h3>
-            <p>Genre: {movieProps.value.movieGenre}</p>
-            <p>Summary: {movieProps.value.movieDescription}</p>
-            <p>Cast: {movieProps.value.movieCast}</p>
-            <a href={movieProps.value.movieWatch_url} target="_blank">
+            <div><img src={movieImg_url} alt={movieImg_url} height="40%" width="40%" /></div>
+            <h2>Name: {movieName}</h2>
+            <h3>Ratings: {movieRating}</h3>
+            <p>Genre: {movieGenre}</p>
+            <p>Summary: {movieDescription}</p>
+            <p>Cast: {movieCast}</p>
+            <a href={movieWatch_url} target="_blank">
                 <button>Watch Now</button>
             </a>
         </li>
