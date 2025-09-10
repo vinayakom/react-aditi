b2bacad7f53daf4f628f4030bc392ea3b7c0a169
Vinayak Patil <vinayakom@gmail.com>
2025-09-10 16:11:36 +0530

    Inline CSS styling added

--- src/components/MovieLists.jsx
+++ src/components/MovieLists.jsx
@@ -1,18 +1,28 @@
-export const MovieLists = ({value}) => {
+export const MovieLists = ({ value }) => {
 
     const { movieImg_url, movieName, movieRating, movieGenre, movieDescription, movieCast, movieWatch_url } = value;
 
+    const buttonStyle = {
+        padding: "1.2rem 2.4rem",
+        border: "none",
+        fontSize: "1.6rem",
+        backgroundColor: "var(--btn-hover-bg-color)",
+        color: "var(--bg-color)",
+    };
+
     return (
-        <li>
+        <li className="card">
             <div><img src={movieImg_url} alt={movieImg_url} height="40%" width="40%" /></div>
-            <h2>Name: {movieName}</h2>
-            <h3>Ratings: {movieRating}</h3>
-            <p>Genre: {movieGenre}</p>
-            <p>Summary: {movieDescription}</p>
-            <p>Cast: {movieCast}</p>
-            <a href={movieWatch_url} target="_blank">
-                <button>Watch Now</button>
-            </a>
+            <div className="card-content">
+                <h2>Name: {movieName}</h2>
+                <h3>Ratings: {movieRating}</h3>
+                <p>Summary: {movieDescription}</p>
+                <p>Genre: {movieGenre.join(", ")}</p>
+                <p>Cast: {movieCast.join(", ")}</p>
+                <a href={movieWatch_url} target="_blank">
+                    <button style={buttonStyle}>Watch Now</button>
+                </a>
+            </div>
         </li>
     );
 };
