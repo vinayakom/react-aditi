aee1d4d428712580ea09def3d7580d911de3a9ec
Vinayak Patil <vinayakom@gmail.com>
2025-09-13 19:06:15 +0530

    Removed the Event handling changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { MovieCards } from "./components/MovieCards";
+import { MovieCards } from "./components/MovieCards";
 //import "./components/MovieCards.css";
 import "./components/MovieCards.module.css";
 import { EventHandling } from "./components/EventHandling";
@@ -6,9 +6,9 @@ import { EventHandling } from "./components/EventHandling";
 export const App = () => {
   return (
     <section className="container">
-      {/* <h1 className="card-heading">Best Movies List</h1>
-      <MovieCards /> */}
-      <EventHandling />
+      <h1 className="card-heading">Best Movies List</h1>
+      <MovieCards />
+      {/* <EventHandling /> */}
     </section>
   );
 };
