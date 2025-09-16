a3240ccb517982f42c33159b02c2b15951ac17d0
Vinayak Patil <vinayakom@gmail.com>
2025-09-16 10:55:07 +0530

    Capturing Propagation changes

--- src/components/EventPropagation.jsx
+++ src/components/EventPropagation.jsx
@@ -12,14 +12,27 @@ export const EventPropagation = () => {
 
     const handleChildClick = (event) => {
         console.log(event);
+        event.stopPropagation();
         console.log("Child Div Clicked");
     }
 
     return (
+        /* Commented part is for bubbling propagation */
+        // <section className="main-div">
+        //     <div className="g-div" onClick={handleGrandParentClick}>
+        //         <div className="p-div" onClick={handleParentClick}>
+        //             <button className="c-div" onClick={handleChildClick}>
+        //                 Child Dev
+        //             </button>
+        //         </div>
+        //     </div>
+        // </section>
+        
+        /* Below part is for capturing propagation */
         <section className="main-div">
-            <div className="g-div" onClick={handleGrandParentClick}>
-                <div className="p-div" onClick={handleParentClick}>
-                    <button className="c-div" onClick={handleChildClick}>
+            <div className="g-div" onClickCapture={handleGrandParentClick}>
+                <div className="p-div" onClickCapture={handleParentClick}>
+                    <button className="c-div" onClickCapture={handleChildClick}>
                         Child Dev
                     </button>
                 </div>
