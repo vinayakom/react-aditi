3dff091651349cb298f54b0fb8924fd5e742ea34
Vinayak Patil <vinayakom@gmail.com>
2025-09-16 16:37:04 +0530

    Parent - Child rendering - State changes

--- src/components/hooks/State.jsx
+++ src/components/hooks/State.jsx
@@ -11,7 +11,7 @@ export const State = () => {
     //console.log(useState());
 
     const [count, setCount] = useState(0);
-
+    console.log("Parent Component Rendered");
     const handleButtonClick = () => {
         setCount(() => count + 1);
         console.log("Button Clicked", count);
@@ -23,6 +23,16 @@ export const State = () => {
                 <h1>Count == {count}</h1>
                 <button onClick={handleButtonClick}>Click to Increment</button>
             </section>
+            <ChildComponent count={count}/>
         </>
     );
 };
+
+function ChildComponent({count}) {
+    console.log("Child Component Rendered");
+    return (
+        <div className="main-div">
+            <h1>Child Component - {count}</h1>
+        </div>
+    );
+}
