77a2661f0d19f7ee59ecc97e64dc99aff5c789b4
Vinayak Patil <vinayakom@gmail.com>
2025-09-16 12:00:45 +0530

    Added count function

--- src/components/hooks/State.jsx
+++ src/components/hooks/State.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 export const State = () => {
-    
-    let value = 0;
+
+    // let value = 0;
+
+    // const handleButtonClick = () => {
+    //     value = value + 1;
+    //     console.log("Button Clicked", value);
+    // };
+    //console.log(useState());
+
+    const [count, setCount] = useState(0);
 
     const handleButtonClick = () => {
-        value = value + 1;
-        console.log("Button Clicked", value);
+        setCount(() => count + 1);
+        console.log("Button Clicked", count);
     };
 
     return (
         <>
-            <h1>Count == {value}</h1>
-            <button onClick={handleButtonClick}>Click to Increment</button>
+            <section className="main-div">
+                <h1>Count == {count}</h1>
+                <button onClick={handleButtonClick}>Click to Increment</button>
+            </section>
         </>
     );
 };
