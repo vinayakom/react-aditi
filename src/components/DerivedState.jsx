3ab2acea92951001ee797f8b16a3c064e7ab42bd
Vinayak Patil <vinayakom@gmail.com>
2025-09-16 17:48:54 +0530

    Derived state changes

--- src/components/DerivedState.jsx
+++ src/components/DerivedState.jsx
@@ -8,8 +8,12 @@ export const DerivedState = () => {
         { name: "Alice", age: 25 },
         { name: "Bob", age: 30 },
         { name: "Charlie", age: 35 },
-        { name: "David", age: 40 },
+        { name: "David", age: 38 },
     ]);
+
+    const userCount = users.length;
+    const averageAge = (users.reduce((sum, user) => sum + user.age, 0) / userCount).toFixed(2);
+    console.log({ users });
 
     return (
         <div className="main-div">
@@ -23,6 +27,8 @@ export const DerivedState = () => {
                     );
                 })}
             </ul>
+            <p>Total Users: {userCount}</p>
+            <p>Average Age: {averageAge}</p>
         </div>
     );
 };
