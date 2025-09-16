f4d0fa863515838d5f2d62da786fe6bbf997f7e6
Vinayak Patil <vinayakom@gmail.com>
2025-09-16 11:30:54 +0530

    State changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // import { MovieCards } from "./components/MovieCards";
-//import "./components/MovieCards.css";
+// import "./components/MovieCards.css";
 // import "./components/MovieCards.module.css";
 // import { EventHandling } from "./components/EventHandling";
 // import { EventProps } from "./components/EventProps";
-//import { EventPropagation } from "./components/EventPropagation";
-import "./components/EventHandling.css  ";
+// import { EventPropagation } from "./components/EventPropagation";
+import "./components/EventHandling.css";
 import { State } from "./components/hooks/State";
 
 export const App = () => {
