b05a445f7d89751fe5fe3b6e76b86121a96dbe43
Vinayak Patil <vinayakom@gmail.com>
2025-09-18 11:12:11 +0530

    Added the toggle details

--- src/projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./ToggleSwitch.css";
 
 export const ToggleSwitch = () => {
+    
+    const [isOn,setIsOn] = useState(false);
+
+    const handleToggleSwitch = () => {
+        setIsOn(!isOn);
+    }
+    
+    const checkIsOn = isOn ? "on" : "off";
+
+    const toggleBackgroundColor = {backgroundColor:isOn ? "#4caf50" : "#f44336"}
+
     return (
-        <div className="toggle-switch">
-            <div className="switch">
-                <span className="switch-state">ON</span>
+        <div 
+            className="toggle-switch" 
+            style={toggleBackgroundColor} 
+            onClick={handleToggleSwitch} 
+        >
+            <div className={`switch ${checkIsOn}`} >
+                <span className="switch-state">{checkIsOn}</span>
             </div>
         </div>
 
