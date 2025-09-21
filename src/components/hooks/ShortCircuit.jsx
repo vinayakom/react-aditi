436642e9fbe184e0f9bd57be6e98e6b70f8989e4
Vinayak Patil <vinayakom@gmail.com>
2025-09-21 14:42:42 +0530

    Short circuit changes includes and, or

--- src/components/hooks/ShortCircuit.jsx
+++ src/components/hooks/ShortCircuit.jsx
@@ -1,15 +1,20 @@
-import "src/index.css";
+import { useState } from "react";
+import "./Hooks.css";
 
 export const ShortCircuitExample = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [user, setUser] = useState();
 
     return (
         <section className="container">
             <h1>Short Circuit Evaluation</h1>
-            <p>You are logged in</p>
+            {isLoggedIn && <p>You are logged in!</p>}
+            {user ? `Hello ${user}` : "Please log in!"}
             <div className="grid-three-cols">
-                <button>Toggle Login State</button>
-                <button>Set User</button>
-                <button>Clear User</button>    
+                <button className="button" onClick={() => setIsLoggedIn(!isLoggedIn)}>Toggle Login State</button>
+                <button className="button" onClick={() => setUser("Aditi")}>Set User</button>
+                <button className="button" onClick={() => setUser("")}>Clear User</button>
             </div>
         </section>
     );
