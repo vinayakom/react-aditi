5012426d7763c58ddeaceacbfb84bb1e30d74907
Vinayak Patil <vinayakom@gmail.com>
2025-09-26 18:18:04 +0530

    useEffect Test

--- src/components/hooks/useEffect/index.jsx
+++ src/components/hooks/useEffect/index.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import "./index.css"
 
 export const ReactUseEffect = () => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("Count value ==> ", count);
+
+    }, [count]);
+
     return (
-        <div>
-            <h1> Hello, useEffect ! </h1>
+        <div className="container effect-container">
+            <h1>useEffect Hook</h1>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
         </div>
     );
 };
