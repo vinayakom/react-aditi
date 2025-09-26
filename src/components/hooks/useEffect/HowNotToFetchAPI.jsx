89aebfb7c7434188ecb50c603ef7c933b9b39927
Vinayak Patil <vinayakom@gmail.com>
2025-09-26 21:16:13 +0530

    Fetch API changes

--- src/components/hooks/useEffect/HowNotToFetchAPI.jsx
+++ src/components/hooks/useEffect/HowNotToFetchAPI.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Pokemon.css"
 
 export const HowNotToFetchAPI = () => {
 
     const [apiData, setApiData] = useState([]);
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => res.json())
-        .then((data) => {
-            //setApiData(data)        
-        })
-        .catch((error) => console.log(error));
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => res.json())
+            .then((data) => {
+                setApiData(data)        
+            })
+            .catch((error) => console.log(error));
+    }, []);
 
     return (
         <div className="container effect-container">
